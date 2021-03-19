df5160eb4ad5ce08170a370d563bb8efd74e6ebc
Oleksii Dehtiarov <odehtiarov@c1r8p8.khpi.ucode-connect.study>
2021-03-19 18:11:53 +0200

    added factorial, exponentiation and work with history, improved logic of calculating

--- race00/js/script.js
+++ race00/js/script.js
@@ -11,23 +11,19 @@ function addNewNumber(num) {
 }
 
 function arithmeticOperation(operation) {
-    if (resultStr.value == "0") {
-        return
-    }
-
     let input = inputStr.innerHTML
 
-    if (input != "" && resultStr.value == "0" && isOperation(input[input.length - 1])) {
+    if (input != "" && resultStr.value == "" && isOperation(input[input.length - 1])) {
         inputStr.innerHTML = input.substring(0, input.length - 1) + operation
     }
     else {
-        inputStr.innerHTML += resultStr.value + operation   
+        inputStr.innerHTML += resultStr.value + operation
         resultStr.value = ""
     }
 }
 
 function equals() {
-    resultStr.value = Number(eval(inputStr.innerHTML + resultStr.value))
+    resultStr.value = Number(eval(replaceHardOperations(inputStr.innerHTML + resultStr.value)))
     inputStr.innerHTML = ""
 }
 
@@ -52,10 +48,87 @@ function reset() {
     inputStr.innerHTML = ""
 }
 
-
-//===============================helping functions===============================//
-function isOperation(operation) {
-    return operation == "+" || operation == "-" || operation == "*" 
-                            || operation == "/" || operation == "+"
+function squareRoot() {
+    if (resultStr.value != "" && resultStr.value > 0) {
+        resultStr.value = Math.pow(Number(resultStr.value), 0.5)
+    }
 }
 
+//-----------------------History-----------------------
+function memoryRecall() {
+    if (resultStr.value != "") {
+        localStorage.setItem("result", resultStr.value)
+    }
+}
+
+function memoryClear() {
+    localStorage.setItem("result", "")
+}
+
+function memoryPlus() {
+    if (localStorage.getItem("result") != "") {
+        localStorage.setItem("result", Number(localStorage.getItem("result")) +
+            Number(resultStr.value))
+
+        resultStr.value = localStorage.getItem("result")
+    }
+}
+
+function memoryMinus() {
+    if (localStorage.getItem("result") != "") {
+        localStorage.setItem("result", Number(localStorage.getItem("result")) -
+            Number(resultStr.value))
+
+        resultStr.value = localStorage.getItem("result")
+    }
+}
+
+
+//===============================helping functions===============================//
+
+
+function isOperation(operation) {
+    return operation == "+" || operation == "-" || operation == "*"
+        || operation == "/" || operation == "+" || operation == "ˆ"
+        || operation == "!"
+}
+
+function replaceHardOperations(input) {
+    let str = ""
+
+    for (let i = 0; i < input.length; i++) {
+        str += input[i]
+        if (isOperation(input[i + 1]) || isOperation(input[i])) {
+            str += " "
+        }
+    }
+
+    let arrStr = str.split(" ")
+    let resArr = []
+
+    for (let i = 0; i < arrStr.length; i++) {
+        if (arrStr[i + 1] == "ˆ") {
+            resArr.push(Math.pow(arrStr[i], arrStr[i + 2]))
+            i += 2
+        }
+        else if (arrStr[i + 1] == "!") {
+            resArr.push(factorial(arrStr[i]))
+            i += 2
+        }
+        else {
+            resArr.push(arrStr[i])
+        }
+    }
+    
+    return resArr.join("")
+}
+
+function factorial(num) {
+    let total = 1
+
+    for (let i = 1; i <= Number(num); i++) {
+        total *= i
+    }
+    return total
+
+}
