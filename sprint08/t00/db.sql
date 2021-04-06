9b767589f0f80c26cb738e15cb84fe0accdc6af1
Aleksios <71853385+dullylol@users.noreply.github.com>
2021-04-06 13:52:03 +0300

    added mysql_native_password

--- sprint08/t00/db.sql
+++ sprint08/t00/db.sql
@@ -1,4 +1,4 @@
-CREATE USER 'odehtiarov'@'localhost' IDENTIFIED BY 'securepass';
+CREATE USER 'odehtiarov'@'localhost' IDENTIFIED WITH mysql_native_password BY 'securepass';
 GRANT ALL PRIVILEGES ON * . * TO 'odehtiarov'@'localhost';
 FLUSH PRIVILEGES;
 CREATE DATABASE ucode_web;
